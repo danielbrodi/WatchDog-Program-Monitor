8598e97cfee11bbd0a9da760281fcb605783d645
Daniel.Brodsky <brod.daniel@gmail.com>
2021-03-22 20:42:27 +0200

    ready-for-review

--- scheduler/include/utils.h
+++ scheduler/include/utils.h
@@ -9,6 +9,13 @@
 #ifndef	__UTILS_H__
 #define	__UTILS_H__
 /******************************************************************************/
+/* escape keys for printf to color output for StdOut */
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+/******************************************************************************/
 
 typedef enum
 {
