a4dba173e5b75984232c32e077eff4e68e535193
Daniel.Brodsky <brod.daniel@gmail.com>
2021-04-07 11:23:52 +0300

    fixes

--- scheduler/source/sorted_list.c
+++ scheduler/source/sorted_list.c
@@ -12,8 +12,8 @@
 #include <stddef.h>		/*	size_t, NULL	*/
 #include <stdlib.h>		/*	malloc, free	*/
 
-#include "utils.h"		/*	status_ty, bolean_ty				*/
-#include "dlist.h"		/*	doubly linked list API wrapper		*/
+#include "utils.h"		/*	status_ty, bolean_ty			*/
+#include "dlist.h"		/*	doubly linked list API wrapper	*/
 #include "sorted_list.h"
 
 /***************************** Macros Definitions *****************************/
@@ -25,12 +25,8 @@
 struct sorted_list
 {
 	dlist_ty *list;	/* doubly linked list with a dummy node in the end of it */
-	Compare_Func_ty *compare_func; /* will sort the elements by its criteria */
+	Compare_Func_ty compare_func; /* will sort the elements by its criteria */
 };
-
-/**************************** Forward Declarations ****************************/
-int CompareElements(const void *data1, const void *data2);
-
 /******************************************************************************/
 /************************* Functions__Implementations *************************/
 /******************************************************************************/
@@ -46,30 +42,22 @@ sorted_list_ty *SortedListCreate(Compare_Func_ty compare_func)
 	if (NULL == new_list->list)
 	{
 		free(new_list);
-		new_list = NULL:
+		new_list = NULL;
 		
 		return(NULL);
 	}
 	
-	new_list->compare = CompareElements;
+	new_list->compare_func = compare_func;
 	
-	return(new_queue);
-}
-/******************************************************************************/
-int CompareElements(const void *data1, const void *data2)
-{
-	assert(data1);
-	assert(data2);
-	
-	return ((int)(long)(data2) - (int)(long)(data1));
+	return(new_list);
 }
 /******************************************************************************/
 void SortedListDestroy(sorted_list_ty *sorted_list)
 {
 	assert(sorted_list);
-	assert(sorted_list_>list);
+	assert(sorted_list->list);
 	
-	DlistDestory(sorted_list->list);
+	DlistDestroy(sorted_list->list);
 	sorted_list->list = NULL;
 	
 	free(sorted_list);
@@ -80,31 +68,31 @@ sorted_list_iter_ty SortedListIteratorBegin(const sorted_list_ty *sorted_list)
 {
 	assert(sorted_list);
 	
-	return (D_ITER_TO_S_ITER(DlistIteratorTobegin(sorted_list->list)));
+	return (D_ITER_TO_S_ITER(DlistIteratorBegin(sorted_list->list)));
 }
 /******************************************************************************/
 sorted_list_iter_ty SortedListIteratorEnd(const sorted_list_ty *sorted_list)
 {
 	assert(sorted_list);
 	
-	return (D_ITER_TO_S_ITER(DlistIteratorTobegin(sorted_list->list)));
+	return (D_ITER_TO_S_ITER(DlistIteratorEnd(sorted_list->list)));
 }
 /******************************************************************************/
 sorted_list_iter_ty SortedListIteratorNext(const sorted_list_iter_ty iter)
 {	
-	return D_ITER_TO_S_ITER(DlistIteratorTonext(S_ITER_TO_D_ITER(iter)));
+	return D_ITER_TO_S_ITER(DlistIteratorNext(S_ITER_TO_D_ITER(iter)));
 }
 /******************************************************************************/
 sorted_list_iter_ty SortedListIteratorPrevious(const sorted_list_iter_ty iter)
 {
-	return D_ITER_TO_S_ITER(DlistIteratorToPrevious(S_ITER_TO_D_ITER(iter)));
+	return D_ITER_TO_S_ITER(DlistIteratorPrevious(S_ITER_TO_D_ITER(iter)));
 }
 /******************************************************************************/
 boolean_ty SortedListIteratorIsEqual(const sorted_list_iter_ty iter_a, 
 const sorted_list_iter_ty iter_b)
 {
 	return (DlistIteratorIsEqual(
-							S_ITER_TO_D_ITER(iter_a), S_ITER_TO_D_ITER(ite_b)));
+							S_ITER_TO_D_ITER(iter_a), S_ITER_TO_D_ITER(iter_b)));
 }
 /******************************************************************************/
 void *SortedListGetData(const sorted_list_iter_ty iter)
@@ -139,10 +127,16 @@ boolean_ty SortedListIsEmpty(const sorted_list_ty *sorted_list)
 	return (DlistIsEmpty(sorted_list->list));
 }
 /******************************************************************************/
+status_ty IsValidElem(void *data, const void *param)
+{
+	++(*(int *)param);
+	return (SUCCESS);
+}
+/******************************************************************************/
 size_t SortedListSize(const sorted_list_ty *sorted_list)
 {
 	size_t counter = 0;
-	sorted_list_iter_ty runner = NULL;
+	sorted_list_iter_ty head = NULL;
 	sorted_list_iter_ty tail = NULL;
 	
 	assert(sorted_list);
@@ -160,7 +154,6 @@ sorted_list_iter_ty SortedListInsert(sorted_list_ty *list, void *data)
 	sorted_list_iter_ty runner = NULL;
 	sorted_list_iter_ty tail = NULL;
 	sorted_list_iter_ty ret = NULL;
-	int status = 1;		/*	for a positive indication	*/
 	
 	assert(list);
 	assert(data);
@@ -168,29 +161,21 @@ sorted_list_iter_ty SortedListInsert(sorted_list_ty *list, void *data)
 	runner = SortedListIteratorBegin(list);
 	tail = SortedListIteratorEnd(list);
 	
-	while (tail != runner && status > 0)
+	while (tail != runner && 
+						list->compare_func(SortedListGetData(runner), data) > 0)
 	{
-		status = list->compare_func(SortedListGetData(runner), data);
 		runner = SortedListIteratorNext(runner);
 	}
 	
-	/* insert as the last element in the list if the runner reached the end */
-	if (SortedListIteratorIsEqual(tail, runner))
-	{
-		return (D_ITER_TO_S_ITER(DlistPushBack(list->list, data)));
-	}
+	ret = D_ITER_TO_S_ITER(DlistInsertBefore(S_ITER_TO_D_ITER(runner), data));
 	
-	ret = D_ITER_TO_S_ITER(DlistInsertBefore(
-				S_ITER_TO_D_ITER(SortedListIteratorPrevious(runner)), data));
-	
-	if (SortedListIteratorIsEqual(ret, SortedListIteratorPrevious(runner)))
+	if (SortedListIteratorIsEqual(ret, runner))
 	{
 		return (tail);	/* insert failed			*/
 	}
 	
 	return (ret); 		/* return the new element	*/
-	
-	
+}
 /******************************************************************************/
 sorted_list_iter_ty SortedListFind(const sorted_list_ty *list, 
                             const sorted_list_iter_ty from_iter, 
@@ -198,8 +183,6 @@ sorted_list_iter_ty SortedListFind(const sorted_list_ty *list,
 												void *param)
 {
 	sorted_list_iter_ty runner = NULL;
-	sorted_list_iter_ty tail = NULL;
-	sorted_list_iter_ty ret = NULL;
 	int status = 1;	/*	for a positive indication	*/
 	
 	assert(list);
@@ -214,7 +197,7 @@ sorted_list_iter_ty SortedListFind(const sorted_list_ty *list,
 	*/
 	while (runner != to_iter && status > 0)
 	{
-		status = list->compare_func(SortedListGetData(runner), data);
+		status = list->compare_func(SortedListGetData(runner), param);
 		runner = SortedListIteratorNext(runner);
 	}
 	
@@ -228,21 +211,20 @@ sorted_list_iter_ty SortedListFind(const sorted_list_ty *list,
 /******************************************************************************/
 sorted_list_iter_ty SortedListFindIf(const sorted_list_iter_ty from_iter, 
 								const sorted_list_iter_ty to_iter, 
-										Is_Match_Func_ty match_func,
-												void *param)
+					Is_Match_Func_ty match_func, void *param)
 {
 	return (D_ITER_TO_S_ITER(
 	DlistFind(S_ITER_TO_D_ITER(from_iter), S_ITER_TO_D_ITER(to_iter),
-														match_func, param)));
+										(IsMatch_Func_ty)match_func, param)));
 }
 /******************************************************************************/
 status_ty SortedListForEach(sorted_list_iter_ty from_iter,
-			const sorted_list_iter_ty to_iter, Action_Funcion_ty action_func,
+			const sorted_list_iter_ty to_iter, Action_Function_ty action_func,
 													 void *param)
 {
 	return (DlistForEach(S_ITER_TO_D_ITER(from_iter), S_ITER_TO_D_ITER(to_iter),
-														action_func, param));
-}													 
+										(Action_Func_ty)action_func, param));
+}				 
 /******************************************************************************/
 void SortedListMerge(sorted_list_ty *dest_list, 
 						sorted_list_ty *src_list)
--- scheduler/test/sorted_list_test.c
+++ scheduler/test/sorted_list_test.c
@@ -1,9 +1,176 @@
-/**********************************FILE-HEADER*********************************\
+/*********************************FILE__HEADER*********************************\
 * File: sorted_list_test.c						 		  								
 * Author: Daniel Brodsky					  								
-* Date: 05/04/2021						   								
+* Date: 07/04/2021						   								
 * Version: Beta				   								
 * Reviewer: Ariel						   								
 * Description: Sorted Linked List API's Functions Testings.		 
 \******************************************************************************/
+
 /********************************* Inclusions *********************************/
+#include <assert.h>	/*	assert	*/
+#include <stddef.h>	/*	size_t	*/
+#include <stdio.h>	/* printf, NULL */
+#include <stdlib.h> /* rand, srand */
+#include <string.h> /* strcmp */
+#include <time.h>	/* time */
+
+#include "utils.h" /* status_ty, boolean_ty, ANSI_COLOR_*/
+#include "sorted_list.h"
+
+/***************************** Macros Definitions *****************************/
+/* print colored output */
+#define PRINT_SUCCESS printf (ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET)
+#define PRINT_FAILURE printf (ANSI_COLOR_RED "FAILURE\n" ANSI_COLOR_RESET)
+
+/* generates random number from 1 to 100 */
+#define RANDOM_NUM (rand() % 100) + 1
+
+#define UNUSED(x) (void)(x)
+/**************************** Forward Declarations ****************************/
+int CompareElements(const void *data1, const void *data2);
+static void SortedListCreateTest(sorted_list_ty *sorted_list);
+static void SortedListDestroyTest(sorted_list_ty *list1, sorted_list_ty *list2);
+static void SortedListIteratorBeginTest(sorted_list_ty *sorted_list);
+static void SortedListInsertTest(sorted_list_ty *sorted_list);
+static void SortedListRemoveTest(sorted_list_ty *sorted_list);
+static void SortedListIsEmptyTest(sorted_list_ty *sorted_list);
+static void SortedListPushFrontTest(sorted_list_ty *sorted_list);
+static void SortedListPushBackTest(sorted_list_ty *sorted_list);
+static void SortedListPopFrontTest(sorted_list_ty *sorted_list);
+static void SortedListPopBackTest(sorted_list_ty *sorted_list);
+static void SortedListSetDataTest(sorted_list_ty *sorted_list);
+/*static status_ty PrintList(void *data, void *param);*/
+static boolean_ty IsMatch(const void *data, void *param);
+/*static void InsertIntToList(sorted_list_ty *sorted_list);*/
+/*static void SortedListForEachTest(sorted_list_ty *sorted_list);*/
+/*static void SortedListFindTest(sorted_list_ty *sorted_list);*/
+/*static void SortedListMultiFindTest(sorted_list_ty *sorted_list, sorted_list_ty *src_list);*/
+/*static void SortedListSpliceTest(sorted_list_ty *sorted_list_dest, sorted_list_ty *sorted_list_src);*/
+/******************************************************************************/
+/******************************* Main__Function *******************************/
+int main()	
+{
+	/* Intializes two empty new sorted lists */
+	sorted_list_ty *dest_list = SortedListCreate(CompareElements);
+	sorted_list_ty *src_list = SortedListCreate(CompareElements);
+	
+	/* Intializes random number generator */
+	srand(time(0));
+	
+	SortedListCreateTest(dest_list);
+	SortedListIteratorBeginTest(dest_list);
+	SortedListInsertTest(dest_list);
+	SortedListRemoveTest(dest_list);
+	SortedListIsEmptyTest(dest_list);
+/*	SortedListPushFrontTest(dest_list);*/
+/*	SortedListPushBackTest(dest_list);*/
+	SortedListPopFrontTest(dest_list);
+	SortedListPopBackTest(dest_list);
+/*	SortedListSetDataTest(dest_list);*/
+/*	InsertIntToList(dest_list);*/
+/*	SortedListForEachTest(dest_list);*/
+/*	SortedListFindTest(dest_list);*/
+/*	SortedListMultiFindTest(dest_list, src_list);*/
+/*	SortedListSpliceTest(dest_list, src_list);*/
+	SortedListDestroyTest(dest_list, src_list);
+	
+	return (0);
+}
+/******************************************************************************/
+/************************Test Functions Implementations************************/
+/******************************************************************************/
+static void SortedListCreateTest(sorted_list_ty *sorted_list)
+{
+	printf("\nSorted List Creation Test: ");
+	NULL == sorted_list ? PRINT_FAILURE : PRINT_SUCCESS;
+}
+/******************************************************************************/
+static void SortedListDestroyTest(sorted_list_ty *list1, sorted_list_ty *list2)
+{
+	SortedListDestroy(list1);
+	SortedListDestroy(list2);
+	printf (ANSI_COLOR_CYAN "\nThe Lists have been deleted\n\n" 
+															ANSI_COLOR_RESET);
+}
+/******************************************************************************/
+
+/******************************************************************************/
+static void SortedListIteratorBeginTest(sorted_list_ty *sorted_list)
+{
+	printf("Iter To Begin/End/IsEqual Test: ");
+	SortedListIteratorIsEqual(SortedListIteratorBegin(sorted_list), SortedListIteratorEnd(sorted_list)) ?
+	 											  PRINT_SUCCESS : PRINT_FAILURE;
+}
+/******************************************************************************/
+static void SortedListInsertTest(sorted_list_ty *sorted_list)
+{	
+	sorted_list_iter_ty new_node = NULL;
+	size_t counter = 2;
+	int num1 = 5, num3 = 4;
+	
+	new_node = SortedListInsert(sorted_list, (void *)(long)num1);
+	printf("\nInsert Test:\nInserting: %d->", (int)(long)SortedListGetData(new_node));
+	
+	new_node = SortedListInsert(sorted_list, (void *)(long)num3);
+	printf("%d->", (int)(long)SortedListGetData(new_node));
+	
+	new_node = SortedListIteratorBegin(sorted_list);
+	printf("Printing Sorted List: ");
+	while (counter > 0)
+	{
+		printf("%d->", (int)(long)SortedListGetData(new_node));
+		new_node = SortedListIteratorNext(new_node);
+		--counter;
+	}
+	printf("\n\n");
+}
+/******************************************************************************/
+static void SortedListRemoveTest(sorted_list_ty *sorted_list)
+{
+
+}
+/******************************************************************************/
+static void SortedListIsEmptyTest(sorted_list_ty *sorted_list)
+{
+	printf("Sorted List IsEmpty Test: ");
+	TRUE == SortedListIsEmpty(sorted_list) ? PRINT_SUCCESS : PRINT_FAILURE;
+}
+/******************************************************************************/
+static void SortedListPushFrontTest(sorted_list_ty *sorted_list)
+{
+}
+/******************************************************************************/
+static void SortedListPushBackTest(sorted_list_ty *sorted_list)
+{
+
+}
+/******************************************************************************/
+static void SortedListPopFrontTest(sorted_list_ty *sorted_list)
+{
+	
+}
+/******************************************************************************/
+static void SortedListPopBackTest(sorted_list_ty *sorted_list)
+{
+	
+}
+/******************************************************************************/
+static void SortedListSetDataTest(sorted_list_ty *sorted_list)
+{
+
+}
+/******************************************************************************/
+int CompareElements(const void *data1, const void *data2)
+{
+	assert(data1);
+	assert(data2);
+	
+	return ((int)(long)(data2) - (int)(long)(data1));
+}
+/******************************************************************************/
+static boolean_ty IsMatch(const void *data, void *param)
+{	
+	return (((int)(long)data == (int)(long)param));
+}
+/******************************************************************************/
