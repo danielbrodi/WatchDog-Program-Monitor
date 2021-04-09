fc4ad32b66f4497188135e5bb38c7d48716b87ef
Daniel.Brodsky <brod.daniel@gmail.com>
2021-04-09 23:05:53 +0300

    Beta Version

--- scheduler/source/UID.c
+++ scheduler/source/UID.c
@@ -6,3 +6,53 @@
 * Reviewer: Omer						   								
 * Description: Unique identifier Functions Implementations.			 
 \******************************************************************************/
+
+/********************************* Inclusions *********************************/
+#include <stddef.h>		/* size_t		*/
+#include <sys/types.h>	/* pid_t		*/
+#include <time.h>		/* time_t		*/
+
+#include "utils.h"		/* boolean_ty	*/
+#include "UID.h"
+/******************************************************************************/
+/************************* Functions__Implementations *************************/
+/******************************************************************************/
+ilrd_uid_ty UIDCreate(void)
+{
+	struct ilrd_uid_ty new_uid;
+	
+	time_t curr_time = time(0);
+	if (-1 == curr_time)
+	{
+		return (UIDGetBadUID());
+	}
+	
+	new_uid.PID = getpid();
+	new_uid.timestamp = curr_time; 
+	++(new_uid.counter); 
+	
+	return(new_uid);
+}
+/******************************************************************************/
+boolean_ty UIDIsEqual(ilrd_uid_ty uid1, ilrd_uid_ty uid2)
+{
+	boolean_ty is_match = TRUE;
+	
+	is_match *= (uid1.PID == uid2.PID);
+	is_match *= (uid1.timestamp == uid2.timestamp);
+	is_match *= (uid1.counter == uid2.counter);
+	
+	return (is_match);
+}
+/******************************************************************************/
+ilrd_uid_ty UIDGetBadUID(void)
+{
+	struct ilrd_uid_ty bad_uid;
+	
+	bad_uid.PID = 0;
+	bad_uid.timestamp = 0; 
+	bad_uid.counter = 0;
+	
+	return (bad_uid);
+}
+/*******************************************************************************
--- scheduler/test/UID_test.c
+++ scheduler/test/UID_test.c
@@ -6,3 +6,32 @@
 * Reviewer: Omer						   								
 * Description: Unique identifier Functions Implementations Testings.		 
 \******************************************************************************/
+
+/********************************* Inclusions *********************************/
+#include <stddef.h>	/*	size_t			*/
+#include <sys/types.h>	/* pid_t		*/
+#include <stdio.h>	/*	printf, NULL	*/
+#include <stdlib.h>	/*	rand, srand		*/
+#include <time.h>	/*	time			*/
+
+#include "utils.h"	/*	status_ty, boolean_ty, ANSI_COLOR_	*/
+
+#include "utils.h"		/* boolean_ty	*/
+#include "UID.h"
+
+/***************************** Macros Definitions *****************************/
+/* print colored output */
+#define PRINT_SUCCESS printf (ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET)
+#define PRINT_FAILURE printf (ANSI_COLOR_RED "FAILURE\n" ANSI_COLOR_RESET)
+/**************************** Forward Declarations ****************************/
+
+/******************************************************************************/
+/******************************* Main__Function *******************************/
+int main()	
+{
+
+}
+/******************************************************************************/
+/************************Test Functions Implementations************************/
+/******************************************************************************/
+
