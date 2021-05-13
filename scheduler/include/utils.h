d4326df5da93fa30c57b85481dea7410dff4ca2e
danielbrodi <broddanik@gmail.com>
2021-05-13 11:26:57 +0300

    Added colors and comments

--- scheduler/include/utils.h
+++ scheduler/include/utils.h
@@ -6,12 +6,28 @@
 #ifndef	__UTILS_H__
 #define	__UTILS_H__
 /******************************************************************************/
+#include <stdio.h>
+#include <assert.h>
+
 /* escape keys for printf to color output for StdOut */
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define CYAN    "\x1b[36m"
+#define RESET_COLOR   "\x1b[0m"
+
+/* print colored output of SUCCESS and FAILURE */
+#define PRINT_SUCCESS printf (ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET)
+#define PRINT_FAILURE printf (ANSI_COLOR_RED "FAILURE\n" ANSI_COLOR_RESET)
+
+#define PRINT_RED printf(ANSI_COLOR_RED)
+#define PRINT_GREEN printf(ANSI_COLOR_GREEN)
+#define PRINT_CYAN printf(ANSI_COLOR_CYAN)
+#define RESET_PRINT_COLOR printf(ANSI_COLOR_RESET)
 /******************************************************************************/
 
 typedef enum
@@ -27,6 +43,5 @@ typedef enum
 	SUCCESS,
 	FAILURE
 } status_ty;
-
 /******************************************************************************/
 #endif	/* __UTILS_H__ */
