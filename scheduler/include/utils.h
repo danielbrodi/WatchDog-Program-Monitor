3c04e8722cbb544be7b8941431a3c64b167cafbb
danielbrodi <broddanik@gmail.com>
2021-05-25 13:03:37 +0300

    added and fixed colors

--- scheduler/include/utils.h
+++ scheduler/include/utils.h
@@ -21,8 +21,11 @@
 
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define PURPLE  "\x1b[35m"
 #define CYAN    "\x1b[36m"
-#define RESET_COLOR   "\x1b[0m"
+#define RESET_COLOR	"\x1b[0m"
 
 /* print colored output of SUCCESS and FAILURE */
 #define PRINT_SUCCESS printf (ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET)
