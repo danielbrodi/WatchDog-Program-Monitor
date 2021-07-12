4d99443d558eb4c5366ac0a19c6f5438d1121c64
danielbrodi <broddanik@gmail.com>
2021-07-12 10:46:49 +0300

    added external program watchdog code

--- watchdog.c
+++ watchdog.c
@@ -4,26 +4,39 @@
 * Date:				11-July-2021
 * Code Reviewer:						   								
 * Version:			1.0		
-* Description:		....TODO....
+* Description:		Watchdog's program design pseudo-code. 
+					Watchdog should protect a client's program from being
+					terminated and restart it if needed. 
+					The client's program should also protect the watchdog
+					program from being terminated and also restart it if needed.
 \******************************************************************************/
 
 /********************************* Pseudo Code ********************************/
 
-/*	struct info
-	{
-		int argc;
-		char *argv[];
-		size_t num_allowed_fails;
-		size_t num_seconds_between_checks;	
-	}
-*/
+/*----------------------------------------------------------------------------*/
+
+/*	a struct that will be transferred from KeepMeAlive to KeepMeAliveIMP with
+ *	all CLI user parameters 	*/
+	 
+	/*	struct info
+		{
+			int argc;
+			char *argv[];
+			size_t num_allowed_fails;
+			size_t num_seconds_between_checks;	
+		}
+	*/
+	
+/*----------------------------------------------------------------------------*/
 	
 /*	KeepMeAlive function - start */
 	/*	asserts */
 	/*	create a new thread and send it KeepMeAliveIMP function with
 	 *	struct info with all the needed info */
-	/*upgrade niceness*/
+	/*	upgrade niceness	*/
 /*	KeepMeAlive - function - end */
+
+/*----------------------------------------------------------------------------*/
 
 /*	KeepMeAliveIMP	function - start */
 	/*	asserts */
@@ -32,26 +45,58 @@
 	/*	save the parameters info in local variables */
 	
 	/*	while program is running */
-		/*	RESET: fork: 	*/
+		/*	fork: 	*/
 			
 			/*	if child: */
 				/*	execv	*/
 				/*	pass cli parmeters */
 				/*	exit if execv fails */
-			/*	end if 		*/
+			/*	end child 		*/
 				
 			/*	if parent:	*/
 				/*	register signal handler that will reset the time */
 				
-				/*	while total time has not passed :*/
-				
-					/*	send signal every num_seconds_between_checks	*/
+				/*	while maximum time has not passed :*/
+					/*	send signal every num_seconds_between_checks using scheduler	*/
 				/*	end of while */
 				
 				/*	terminate child process (watchdog) */
+			/*	end parent */
 	/*	end of while program is runnig */
-			
-			
-		/*	end if */
-	/*	
+/*	KeepMeAliveIMP function - end	*/
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*-----	WATCHDOG - EXTERNAL PROGRAM -----*/
+
+/*	create a variable that tracks the number of missed signals */
+
+/*	register signal handler that will reset the time that has been passed since the last signal that was send */
+
+/*	while program runs */
+
+	/*	while number_missed_signals < num_allowed_fails */
+
+		/*	send signal each num_seconds_between_checks using a scheduler */
+		
+		/*	while sleep(num_seconds_between_checks + 30 seconds) */
+				
+				/*	if woke up : */
+					/*	right signal was received = break sleep loop */
+					/*	wrong signal was received = continue sleep */
+		
+		/*	end of while sleep */
+		
+		/*	if no more time left to sleep: increment number_missed_signals counter */
+		
+	/*	end of while number_missed_signals - which means reached maximum fails that allowed */
 	
+	/*	restart user's program using argc argv parameters that were received through execv */
+	
+	/*	restart number_missed_signals counter */
+	
+/*	end while program runs */
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
